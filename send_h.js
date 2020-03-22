130ca8d68fb7c18ff57e49034a81baef19e12652
codefooo <codefooo@foo.com>
2020-03-22 16:03:36 +0900

    .

--- send_h.js
+++ send_h.js
@@ -4,7 +4,7 @@ module.exports = function(callback) {
     web3.eth.getAccounts().then((accounts, error) => {
         web3.eth.sendTransaction({from:accounts[0],
                                     to: "0xEf0F089f0D03fF4CC1a3188068D190109126379F",
-                                 value:web3.utils.toWei("10", "ether")}, callback);
+                                 value:web3.utils.toWei("10", "ether")}, callback());
     });
 
 }
--- send_u.js
+++ send_u.js
@@ -4,7 +4,7 @@ module.exports = function(callback) {
     web3.eth.getAccounts().then((accounts, error) => {
         web3.eth.sendTransaction({from:accounts[2],
                                     to: "0x547d73355A851079E0395aDB2C647821b74C7eAF",
-                                 value:web3.utils.toWei("10", "ether")}, callback);
+                                 value:web3.utils.toWei("10", "ether")}, callback());
     });
 
 }
